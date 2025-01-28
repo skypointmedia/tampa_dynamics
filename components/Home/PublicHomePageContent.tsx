052b79dbcdd3a0cd8bfcc076d218db4f8aa40ded
Matt Santucci <info@skypoint.media>
2025-01-28 17:01:51 -0500

    lint errors

--- components/Home/PublicHomePageContent.tsx
+++ components/Home/PublicHomePageContent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-  import { BoltIcon, UsersIcon } from '@heroicons/react/24/outline'
+import { BoltIcon, UsersIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'  
   const primaryFeatures = [
     {
@@ -55,7 +55,7 @@ import Image from 'next/image'
       {
         name: 'X',
         href: '#',
-        icon: (props) => (
+        icon: (props: React.SVGProps<SVGSVGElement>) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
           </svg>
@@ -64,7 +64,7 @@ import Image from 'next/image'
       {
         name: 'GitHub',
         href: '#',
-        icon: (props) => (
+        icon: (props: React.SVGProps<SVGSVGElement>) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path
               fillRule="evenodd"
