8461fc4e80cad99cd073775ce623fd719f9dfc97
Matt Santucci <info@skypoint.media>
2025-01-28 12:59:57 -0500

    AWS Backend config

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,46 @@
+"use client";
 
-export default function Home() {
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./globals.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+// import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  function listTodos() {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({
+      content: window.prompt("Todo content"),
+    });
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Tampa Dynamics</h1>
-    </div>
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      
+    </main>
   );
 }
