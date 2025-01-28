4c9fe899d0ca9fc66b28fe00f9f3c3d28b3848f9
Matt Santucci <info@skypoint.media>
2025-01-28 12:29:49 -0500

    Initial project commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tampadynamics": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
